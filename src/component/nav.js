27afba003c44c1b1adbf0830ca9bfd5a96d615c3
anas badri <royalanas01@gmail.com>
2024-06-17 13:33:35 -0700

    Update nav.js

--- src/component/nav.js
+++ src/component/nav.js
@@ -16,9 +16,9 @@ function Navbar() {
 		<header>
 			<h3>An aS</h3>
 			<nav ref={navRef}>
-				<a href="/#home">Home</a>
-				<a href="/#skills">My skills</a>
-				<a href="/#project">Project</a>
+				<a href="#home">Home</a>
+				<a href="#skills">My skills</a>
+				<a href="#project">Project</a>
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
